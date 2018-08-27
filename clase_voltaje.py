5ddf696003f1ee6d4a3e0ee290bcc6da94ae8bb1
Matías Zanini <matias.zanini@outlook.com>
2018-08-27 02:42:22 -0300

    Objeto señal constante
    Le agregue a la clase el objeto para emitir una señal constante a un valor fijo suponiendo que la placa entrega de 0 a 5 volts. Aparentemente esta tardando mas tiempo del que debería. (si le pido 0.5 segundos, tarda mucho mas). Habría que ver por qué pasa esto.

--- clase_voltaje.py
+++ clase_voltaje.py
@@ -21,16 +21,16 @@ class Señal:
 
     #---------------Emision--------------------------
 
-    #Señal armonica de amplitud V, frecuencia f, duracion T.
+    
 
-    def Armonica(V,f,T):
+    def Armonica(freq_arm,dur_arm):  #Señal armonica de amplitud 256 bits (suponemos 5V), frecuencia freq_arm, duracion dur_arm.
 
         onda=''
-        cant_puntos = int(BITRATE * T)
+        cant_puntos = int(BITRATE * dur_arm)
         silencios = cant_puntos % BITRATE
         
         for x in range(cant_puntos):
-            onda += chr(int(math.sin(x / ((BITRATE / f) / math.pi)) * 127 + 128))
+            onda += chr(int(math.sin(x / ((BITRATE / freq_arm) / math.pi)) * 127 + 128))
             
         #Llenamos lo que falta de duracion con silencios
         for x in range(silencios): 
@@ -57,6 +57,64 @@ class Señal:
         stream.stop_stream()
         stream.close()
         p.terminate()
+        
+    def Cte(amp_cte,dur_cte):  #Señal constante de voltaje amp_cte y duracion dur_cte, asumiendo que la placa entrega de 0V a 5V
+        
+        if amp_cte <=5:
+            cant_puntos = int(BITRATE * dur_cte)
+            silencios = cant_puntos % BITRATE
+            amp_bit= amp_cte*256/5 #convierte de volts a bits
+            señal_cte=np.zeros(cant_puntos)+amp_bit
+            
+            #Llenamos lo que falta de duracion con silencios
+            señal_cte= np.append(señal_cte, np.zeros(silencios))
+            
+            señal_cte=señal_cte.astype(np.float32).tostring()
+            
+            
+            
+        
+            p = pa()
+            stream = p.open(
+                       format=p.get_format_from_width(1),
+                       channels=1,
+                       rate=BITRATE,
+                       output=True,
+                       )
+            stream.write(señal_cte)
+            stream.stop_stream()
+            stream.close()
+            p.terminate()
+        else:
+            return ('El voltaje debe ser menor a 5V')
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
         
     
     
