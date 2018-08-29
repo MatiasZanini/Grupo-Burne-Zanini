c802ee248839f8a099203afe86fdacf2e382cafe
Matías Zanini <42555461+MatiasZanini@users.noreply.github.com>
2018-08-29 20:04:54 -0300

    quitamos clase, redefinimos funciones
    Separamos la forma de la señal de la funcion emitir. Agregamos a la funcion emitir, la posibilidad de hacerlo en modo callback.

--- clase_voltaje.py
+++ clase_voltaje.py
@@ -10,45 +10,70 @@ Created on Sun Aug 26 17:15:27 2018
 #Importamos las librerías y seteamos el rate.
 
 import numpy as np
-import matplotlib.pyplot as plt
+#import matplotlib.pyplot as plt
 import pyaudio
 from pyaudio import PyAudio as pa
 import math
-import wave
+import wave 
+import time
 
 BITRATE=44100
 
 #%%
-class Señal:
-
-    #---------------Emision--------------------------
+   #---------------Emision--------------------------
 
     
 
-    def Armonica(freq_arm,dur_arm):  #Señal armonica de amplitud 256 bits (suponemos 5V), frecuencia freq_arm, duracion dur_arm.
+def armonica(freq_arm,dur_arm):  #Señal armonica de amplitud 256 bits (suponemos 5V), frecuencia freq_arm, duracion dur_arm.
 
-        onda=''
-        cant_puntos = int(BITRATE * dur_arm)
-        silencios = cant_puntos % BITRATE
+    onda=''
+    cant_puntos = int(BITRATE * dur_arm)
+    silencios = cant_puntos % BITRATE
+    
+    for x in range(cant_puntos):
+        onda += chr(int(math.sin(x / ((BITRATE / freq_arm) / math.pi)) * 126/2 + 128/2))
         
-        for x in range(cant_puntos):
-            onda += chr(int(math.sin(x / ((BITRATE / freq_arm) / math.pi)) * 127 + 128))
+    #Llenamos lo que falta de duracion con silencios
+    for x in range(silencios): 
+        onda += chr(128)
+    
+    #dom= np.array(range(cant_puntos))
+    
+    #grafica la señal enviada
+    #        onda_plot=np.sin(dom)*127+128
+    #        plt.figure(1)
+    #        plt.plot(dom,onda_plot)
+    #        plt.xlabel('Frame')
+    #        plt.ylabel('Intensidad')
+
+#ejecuta la señal
+    
+def emitir(onda,bitrate,callback=None):    
+    
             
-        #Llenamos lo que falta de duracion con silencios
-        for x in range(silencios): 
-            onda += chr(128)
+    p = pa()
+    if callback:
+        stream = p.open(
+        format=p.get_format_from_width(1),
+        channels=1,
+        rate=BITRATE,
+        output=True,
+        stream_callback=callback)
+        stream.start_stream()
+
+        # wait for stream to finish (5)
+        while stream.is_active():
+            time.sleep(0.1)
         
-        dom= np.array(range(cant_puntos))
+        # stop stream (6)
+        stream.stop_stream()
+        stream.close()
         
-        #grafica la señal enviada
-        onda_plot=np.sin(dom)*127+128
-        plt.figure(1)
-        plt.plot(dom,onda_plot)
-        plt.xlabel('Frame')
-        plt.ylabel('Intensidad')
         
-        #ejecuta la señal
-        p = pa()
+        # close PyAudio (7)
+        p.terminate()
+
+    else:
         stream = p.open(
             format=p.get_format_from_width(1),
             channels=1,
@@ -58,85 +83,97 @@ class Señal:
         stream.write(onda)
         stream.stop_stream()
         stream.close()
-        p.terminate()
-        
-    def Cte(amp_cte,dur_cte):  #Señal constante de voltaje amp_cte y duracion dur_cte, asumiendo que la placa entrega de 0V a 5V
-        
-        if amp_cte <=5:
-            cant_puntos = int(BITRATE * dur_cte)
-            silencios = cant_puntos % BITRATE
-            amp_bit= amp_cte*256/5 #convierte de volts a bits
-            señal_cte=np.zeros(cant_puntos)+amp_bit
-            
-            #Llenamos lo que falta de duracion con silencios
-            señal_cte= np.append(señal_cte, np.zeros(silencios))
-            
-            señal_cte=señal_cte.astype(np.float32).tostring()
-            
-            
-            
-        
-            p = pa()
-            stream = p.open(
-                       format=p.get_format_from_width(1),
-                       channels=1,
-                       rate=BITRATE,
-                       output=True,
-                       )
-            stream.write(señal_cte)
-            stream.stop_stream()
-            stream.close()
-            p.terminate()
-        else:
-            return ('El voltaje debe ser menor a 5V')
+    p.terminate()
+    
         
         
+#def Cte(amp_cte,dur_cte):  #Señal constante de voltaje amp_cte y duracion dur_cte, asumiendo que la placa entrega de 0V a 5V
+#    
+#    if amp_cte <=1.20:
+#        cant_puntos = int(BITRATE * dur_cte)
+#        silencios = cant_puntos % BITRATE
+#        amp_bit= amp_cte*256/1.20 #convierte de volts a bits
+#        señal_cte_lista=list(np.zeros(cant_puntos)+amp_bit)
+#        
+#        señal_cte=''
+#        
+#        for x in range(cant_puntos):
+#            señal_cte += chr(int(señal_cte_lista[x]))
+#            
+#        for x in range(silencios):
+#            señal_cte += chr(128)
+##            
+##            #Llenamos lo que falta de duracion con silencios
+##            señal_cte= np.append(señal_cte, np.zeros(silencios))
+##            
+##            señal_cte=señal_cte.astype(np.float32).tostring()
         
-        #------------Medicion-------------------------------------
         
-    def Medir(dur_med):        #Devuelve un array con una medicion de voltaje de duracion dur_med.
-        FORMAT = pyaudio.paInt16
-        CHANNELS = 1   #creo que si ponemos 2 es estereo
-        #RATE = 44100
-        CHUNK = 1024          #Espacio que ocupa un bloque de datos del buffer. La señal se divide en estos "chunks".
-        #RECORD_SECONDS = 5
-        nombre_arch = 'arch.wav'
+#            
+        
+        
+#    
+#        p = pa()
+#        stream = p.open(
+#                   format=p.get_format_from_width(1),
+#                   channels=1,
+#                   rate=BITRATE,
+#                   output=True,
+#                   )
+#        stream.write(señal_cte)
+#        stream.stop_stream()
+#        stream.close()
+#        p.terminate()
+#    else:
+#        return ('El voltaje debe ser menor a 1.20V')
+    
+    
+    
+    #------------Medicion-------------------------------------
+    
+def medir(dur_med):        #Devuelve un array con una medicion de voltaje de duracion dur_med.
+    FORMAT = pyaudio.paInt16
+    CHANNELS = 1   #creo que si ponemos 2 es estereo
+    #RATE = 44100
+    CHUNK = 1024          #Espacio que ocupa un bloque de datos del buffer. La señal se divide en estos "chunks".
+    #RECORD_SECONDS = 5
+    nombre_arch = 'arch.wav'
  
-        p = pa()
+    p = pa()
  
 # Empieza a grabar
-        stream = p.open(format=FORMAT, channels=CHANNELS,
-                rate=BITRATE, input=True,
-                frames_per_buffer=CHUNK)
-        print('grabando...')
-        frames = []
+    stream = p.open(format=FORMAT, channels=CHANNELS,
+            rate=BITRATE, input=True,
+            frames_per_buffer=CHUNK)
+    print('grabando...')
+    frames = []
  
-        for i in range(0, int(BITRATE / CHUNK * dur_med)):
-            data = stream.read(CHUNK)
-        frames.append(data)
-        print('finalizando grabación...')
-        
-        
+    for i in range(0, int(BITRATE / CHUNK * dur_med)):
+        data = stream.read(CHUNK)
+    frames.append(data)
+    print('finalizando grabación...')
+    
+    
 # Termina de grabar
-        stream.stop_stream()
-        stream.close()
-        p.terminate()
+    stream.stop_stream()
+    stream.close()
+    p.terminate()
  
-    #Crea un archivo temporal .wav para poder recuperarlo como array mas tarde.
-        waveFile = wave.open(nombre_arch, 'wb')
-        waveFile.setnchannels(CHANNELS)
-        waveFile.setsampwidth(p.get_sample_size(FORMAT))
-        waveFile.setframerate(BITRATE)
-        waveFile.writeframes(b''.join(frames))
-        waveFile.close()
-        
-        arch_temp = wave.open('arch.wav','r')
+#Crea un archivo temporal .wav para poder recuperarlo como array mas tarde.
+    waveFile = wave.open(nombre_arch, 'wb')
+    waveFile.setnchannels(CHANNELS)
+    waveFile.setsampwidth(p.get_sample_size(FORMAT))
+    waveFile.setframerate(BITRATE)
+    waveFile.writeframes(b''.join(frames))
+    waveFile.close()
+    
+    arch_temp = wave.open('arch.wav','r')
 
 #Extrae un array de la señal wav
-        señal = arch_temp.readframes(-1)
-        señal = np.fromstring(señal, 'Int16')
+    señal = arch_temp.readframes(-1)
+    señal = np.fromstring(señal, 'Int16')
 
-        return señal
+    return señal
 
 ##If Stereo
 #if spf.getnchannels() == 2:       ---------esta sentencia impide ingresar dos canales. Chequear si es necesaria------
