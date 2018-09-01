8237098c21bd7788a52e6e52fe37b56800d232dd
sofiaburne <42590422+sofiaburne@users.noreply.github.com>
2018-09-01 17:03:00 -0300

    reorden del script
    No hay nigún cambio más que un poco de reorganización

--- clase_voltaje.py
+++ clase_voltaje.py
@@ -1,13 +1,4 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Sun Aug 26 17:15:27 2018
-
-@author: Mati
-"""
-
-#%%
-
-#Importamos las librerías y seteamos el rate.
+#Importamos las librerías
 
 import numpy as np
 #import matplotlib.pyplot as plt
@@ -17,12 +8,17 @@ import math
 import wave 
 import time
 
+#%% Seteamos el rate
+
 BITRATE=44100
 
-#%%
+#%% Creamos funciones para comunicarnos con la placa de audio
+
+
    #---------------Emision--------------------------
 
-    
+#---------------------------------------------------------------------------------------------------------------------------
+#Tipos de ondas    
 
 def armonica(freq_arm,dur_arm):  #Señal armonica de amplitud 256 bits (suponemos 5V), frecuencia freq_arm, duracion dur_arm.
 
@@ -37,6 +33,8 @@ def armonica(freq_arm,dur_arm):  #Señal armonica de amplitud 256 bits (suponemo
     for x in range(silencios): 
         onda += chr(128)
     
+    return onda;
+    
     #dom= np.array(range(cant_puntos))
     
     #grafica la señal enviada
@@ -46,55 +44,13 @@ def armonica(freq_arm,dur_arm):  #Señal armonica de amplitud 256 bits (suponemo
     #        plt.xlabel('Frame')
     #        plt.ylabel('Intensidad')
 
-#ejecuta la señal
-    
-def emitir(onda,bitrate,callback=None):    
-    
-            
-    p = pa()
-    if callback:
-        stream = p.open(
-        format=p.get_format_from_width(1),
-        channels=1,
-        rate=BITRATE,
-        output=True,
-        stream_callback=callback)
-        stream.start_stream()
-
-        # wait for stream to finish (5)
-        while stream.is_active():
-            time.sleep(0.1)
-        
-        # stop stream (6)
-        stream.stop_stream()
-        stream.close()
-        
-        
-        # close PyAudio (7)
-        p.terminate()
-
-    else:
-        stream = p.open(
-            format=p.get_format_from_width(1),
-            channels=1,
-            rate=BITRATE,
-            output=True,
-            )
-        stream.write(onda)
-        stream.stop_stream()
-        stream.close()
-    p.terminate()
-    
-        
-        
-#def Cte(amp_cte,dur_cte):  #Señal constante de voltaje amp_cte y duracion dur_cte, asumiendo que la placa entrega de 0V a 5V
+#def Cte(amp_cte,dur_cte):  #Señal constante de voltaje amp_cte y duracion dur_cte, asumiendo que la placa entrega de 0V a 1.2V
 #    
 #    if amp_cte <=1.20:
 #        cant_puntos = int(BITRATE * dur_cte)
 #        silencios = cant_puntos % BITRATE
 #        amp_bit= amp_cte*256/1.20 #convierte de volts a bits
 #        señal_cte_lista=list(np.zeros(cant_puntos)+amp_bit)
-#        
 #        señal_cte=''
 #        
 #        for x in range(cant_puntos):
@@ -107,11 +63,6 @@ def emitir(onda,bitrate,callback=None):
 ##            señal_cte= np.append(señal_cte, np.zeros(silencios))
 ##            
 ##            señal_cte=señal_cte.astype(np.float32).tostring()
-        
-        
-#            
-        
-        
 #    
 #        p = pa()
 #        stream = p.open(
@@ -126,8 +77,46 @@ def emitir(onda,bitrate,callback=None):
 #        p.terminate()
 #    else:
 #        return ('El voltaje debe ser menor a 1.20V')
+
+
+#---------------------------------------------------------------------------------------------------------------------------
+#Ejecución de la señal de emisión
+    
+def emitir(onda,bitrate,callback=None):    
+              
+    p = pa()
+    if callback: #modo callback
+        stream = p.open(
+        format=p.get_format_from_width(1),
+        channels=1,
+        rate=BITRATE,
+        output=True,
+        stream_callback=callback)
+        stream.start_stream()
+
+        while stream.is_active():
+            time.sleep(0.1)
+        
+        stream.stop_stream()
+        stream.close()
+        p.terminate()
+
+    else: #modo bloqueo
+        stream = p.open(
+            format=p.get_format_from_width(1),
+            channels=1,
+            rate=BITRATE,
+            output=True,
+            )
+        stream.write(onda)
+        stream.stop_stream()
+        stream.close()
+   
+    p.terminate()
     
     
+    
+#---------------------------------------------------------------------------------------------------------------------------
     
     #------------Medicion-------------------------------------
     
@@ -175,6 +164,9 @@ def medir(dur_med):        #Devuelve un array con una medicion de voltaje de dur
 
     return señal
 
+
+#---------------------------------------------------------------------------------------------------------------------------
+
 ##If Stereo
 #if spf.getnchannels() == 2:       ---------esta sentencia impide ingresar dos canales. Chequear si es necesaria------
 #    print 'Just mono files'
@@ -191,9 +183,7 @@ def medir(dur_med):        #Devuelve un array con una medicion de voltaje de dur
     #queda chequear que este midiendo bien con el microfono y/o el cable del labo. Hay que ver si mide en bits (de 0 a 255), en cuyo
     #caso agregar la siguiente linea:    señal=señal*5/255   (suponiendo que la placa entrega de 0 a 5V)
         
-        
-        
-        
+
         
         
         
---
+++ emision_medicion.py
@@ -0,0 +1,12 @@
+#importamos módulo para comunicarnos con la placa
+import clase_voltaje.py
+
+#%% Generamos una señal (modo callback)
+
+señal = armonica(100,120) #esto lo elegimos según la señal que querramos
+
+def callback_emision(in_data, frame_count, time_info, status):  #stream_callback pide una función de 4 argumentos.
+		data = señal.readframes(frame_count) #pedazo de señal
+		return (data, pyaudio.paContinue)        
+        
+        
