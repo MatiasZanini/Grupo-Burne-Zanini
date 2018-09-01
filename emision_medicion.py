5f6a2e6a17ba88f5aa57981be3edc698405bb58f
sofiaburne <42590422+sofiaburne@users.noreply.github.com>
2018-09-01 18:32:04 -0300

    script que para emitir y medir
    Implenta el modulo que armamos en clase_voltaje.py para comunicarnos con la placa, pero en este HACEMOS las emisiones/mediciones.
    Ver el tema de la función callback_medicion

--- emision_medicion.py
+++ emision_medicion.py
@@ -1,12 +1,40 @@
-#importamos módulo para comunicarnos con la placa
+#importamos módulo para comunicarnos con la placa y librerias
 import clase_voltaje.py
 
-#%% Generamos una señal (modo callback)
+import numpy as np
+import matplotlib.pyplot as plt
+import pyaudio
+from pyaudio import PyAudio as pa
+import wave 
+
+
+#%% Generamos una señal
 
 señal = armonica(100,120) #esto lo elegimos según la señal que querramos emitir
 
+#modo callback
+
 def callback_emision(in_data, frame_count, time_info, status):  #stream_callback pide una función de 4 argumentos.
 		data = señal.readframes(frame_count) #pedazo de señal
-		return (data, pyaudio.paContinue)        
-        
-        
+		return (data, pyaudio.paContinue)   
+
+emision_call = emitir(BITRATE, callback_emision)     
+
+#modo bloqueo
+
+#emision_block = emitir(señal, BITRATE)
+
+
+#%% Medimos señal
+
+#modo callback
+
+def callback_medición(in_data, frame_count, time_info, status):
+    return (in_data, pyaudio.paContinue)
+# no se si está bien así la callback para medir
+    
+medicion_call = medir(500, callback_medicion)
+
+#modo bloqueo
+
+#medicion_block = medir(500)
